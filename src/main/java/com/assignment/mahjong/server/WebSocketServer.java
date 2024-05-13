88c181c7f8e55c44f381c108f91208cbb530f23d
amadeuscriss <amadeuscriss@gmail.com>
2024-05-13 15:04:52 +0800

    更改结构

--- src/main/java/com/assignment/mahjong/server/WebSocketServer.java
+++ src/main/java/com/assignment/mahjong/server/WebSocketServer.java
@@ -1,16 +1,23 @@
 package com.assignment.mahjong.server;
 
 
+
 import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+
 @ServerEndpoint("/websocket")
 @Component
 public class WebSocketServer {
+
+    public static final Logger log = LoggerFactory.getLogger(WebSocketServer.class);
 
     /**
      *  记录当前连接个数
@@ -21,7 +28,9 @@ public class WebSocketServer {
      * 连接建立成功调用的方法
      */
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessionMap.put(username, session);
+        log.info("有新用户加入， username = {}， 当前在线人数{}", username, sessionMap.size());
         System.out.println("WebSocket opened: " + session.getId());
     }
 
@@ -50,4 +59,16 @@ public class WebSocketServer {
         System.err.println("WebSocket error: ");
         error.printStackTrace();
     }
+
+    private void sendMessageToAll(String message) {
+        try {
+            for (Session session : sessionMap.values()) {
+                log.info("Sending message to all clients: " + session.getId(), message);
+                session.getBasicRemote();
+            }
+        } catch (Exception e) {
+            log.error("Error sending message to all clients: " + e);
+        }
+    }
+
 }
