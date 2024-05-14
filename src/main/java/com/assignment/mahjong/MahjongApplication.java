01e991443bc48553a72a3df8fd9ed72de10e8b73
amadeuscriss <amadeuscriss@gmail.com>
2024-05-14 10:31:17 +0800

    更改结构

--- src/main/java/com/assignment/mahjong/MahjongApplication.java
+++ src/main/java/com/assignment/mahjong/MahjongApplication.java
@@ -2,6 +2,9 @@ package com.assignment.mahjong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MahjongApplication {
@@ -10,4 +13,17 @@ public class MahjongApplication {
         SpringApplication.run(MahjongApplication.class, args);
     }
 
+
+    @Configuration
+    public class CrosConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET","HEAD","POST","DELETE","OPTIONS","PUT")
+                    .allowCredentials(true)
+                    .maxAge(3600)
+                    .allowedHeaders("*");
+        }
+    }
 }
--- views/src/main.js
+++ views/src/main.js
@@ -1,4 +1,6 @@
+import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from "./plugins/axios";
 
 createApp(App).mount('#app')
