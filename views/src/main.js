0849a042d17d160bc3559e96356201a95812ad50
amadeuscriss <amadeuscriss@gmail.com>
2024-05-14 16:17:42 +0800

    vue部分连接websocket准备工作

--- views/src/main.js
+++ views/src/main.js
@@ -4,3 +4,12 @@ import App from './App.vue'
 import axios from "./plugins/axios";
 
 createApp(App).mount('#app')
+import websocket from 'vue-native-websocket';
+
+Vue.use(websocket, '', {
+    connectManually: true, // 手动连接
+    format: 'json', // json格式
+    reconnection: true, // 是否自动重连
+    reconnectionAttempts: 5, // 自动重连次数
+    reconnectionDelay: 2000, // 重连间隔时间
+});
